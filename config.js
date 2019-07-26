a265d437b93e9d9a02606bf3ac917b5df4d0dff5
Olya <olga.akinshyna@gmail.com>
2019-07-26 15:49:32 +0300

    config example

---
+++ config.js
@@ -0,0 +1,22 @@
+const defaultConfig = {
+    timer: 3000,
+    fonts : [
+        {
+            name: 'Hack', 
+            source: 'https://cdnjs.cloudflare.com/ajax/libs/hack-font/3.003/web/fonts/hack-regular.woff',
+            descriptor: {}
+        },
+        {
+            name: 'Hanalei',
+            source: './fonts/Hanalei-Regular.ttf',
+            descriptor: {}
+        },
+        {
+            name: 'NotoSans',
+            source: './fonts/NotoSans-Regular.woff2',
+            descriptor: {}
+        }
+    ]
+}
+
+export { defaultConfig }
